83a6c16c2b118b5cc35080d86577726547843faa
dchen71 <dchen12812@gmail.com>
2015-07-05 13:50:11 -0700

    clear function and more filtering logic for the answer

--- Scripts/calculator.js
+++ Scripts/calculator.js
@@ -1,25 +1,11 @@
-function add(v1,v2){
-  return v1 + v2;
-}
-
-function multiply(v1,v2){
-  return v1 * v2;
-}
-
-function divide(v1,v2){
-  return v1 / v2;
-}
-
-function subtract(v1,v2){
-  return v1-v2;
-}
-
+//Global variables
 var operator = null;
 var num1 = null;
 var num2 = null;
 var answer = 0;
 var turn = 1;
 
+//Creates the values of the numbers
 function num(val){
 	if(turn == 1)
 		if(num1 == null)
@@ -36,6 +22,7 @@ function num(val){
 	console.log(num2);
 }
 
+//Detects which operator will be used and also adjusts next number value
 function oper(val){
 	operator = val;
 	if(num1 != null)
@@ -47,24 +34,56 @@ function answer(){
 	document.getElementsByClassName("answer").value = answer;
 }
 
+//Finds the answer and clears values
 function clr_eq(){
-	num1 = parseInt(num1);
-	num2 = parseInt(num2);
-	switch(operator){
-		case '+':
-			answer = add(num1, num2);
-			break;
-		case '-':
-			answer = subtract(num1, num2);
-			break;
-		case '/':
-			answer = divide(num1, num2);
-			break;
-		case '*':
-			answer = multiply(num1, num2);
-			break;
+	if (num1 != null && num2 != null){
+		num1 = parseInt(num1);
+		num2 = parseInt(num2);
+		switch(operator){
+			case '+':
+				answer = add(num1, num2);
+				break;
+			case '-':
+				answer = subtract(num1, num2);
+				break;
+			case '/':
+				answer = divide(num1, num2);
+				break;
+			case '*':
+				answer = multiply(num1, num2);
+				break;
+		}
+		clear();
 	}
+	else
+		clear();
+
 
 	console.log(answer);
 
+}
+
+//Resets the data back to the beginning
+function clear(){
+	num1 = null;
+	num2 = null;
+	turn = 1;
+	operator = null;
+}
+
+//Main logic for calculations
+function add(v1,v2){
+  return v1 + v2;
+}
+
+function multiply(v1,v2){
+  return v1 * v2;
+}
+
+function divide(v1,v2){
+  return v1 / v2;
+}
+
+function subtract(v1,v2){
+  return v1-v2;
 }
